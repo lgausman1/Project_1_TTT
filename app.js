94abd57b6da8a0ea344dfae3afef93ab1d5175b3
lgausman1 <l.gausman@comcast.net>
2015-05-10 21:26:16 -0700

    added getWinner and refactored evaluating functions into it

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 $(document).ready(function() {
 
-
     function Game() {
     //Create the board
-    //var self = this;
     this.board = new Board();
  
     //Create a new instance of player 1
@@ -11,7 +9,7 @@ $(document).ready(function() {
     
     //Do the same for a player 2
     this.player2 = new Player("o");
-
+    // set up turn counter
     var turncount = this.turncount;
     this.turncount = 0;
  
@@ -21,6 +19,12 @@ $(document).ready(function() {
 
       var _this = this;
       var $gameMsgDiv = $('#game_msg');
+
+      function getWinner(player) {
+        rowWinner(player);
+        colWinner(player);
+        diagWinner(player);
+      }
 
       function rowWinner(player) {
         if($('#a-1').hasClass(player) && $('#a-2').hasClass(player) && $('#a-3').hasClass(player)) {
@@ -61,20 +65,18 @@ $(document).ready(function() {
       }
 
       this.board.cells.on('click', function() {
-      // assign turn add class, remove 'open' to prevent reclicking
+      // assign turn, add class, remove 'open' to prevent reclicking
+      // if turncount is even play x
         if(_this.turncount % 2 === 0 && $(this).hasClass('open')) {
-          $(this).html('X').addClass('xBox').removeClass('open');
+          // remove 'open'class to prevent player from clicking again
+          $(this).html('<p>X</p>').addClass('xBox').removeClass('open');
           // evaluate win
-          rowWinner('xBox');
-          colWinner('xBox');
-          diagWinner('xBox');
-          
-        } else if(_this.turncount % 2 !== 0 && $(this).hasClass('open')) {
-          $(this).html('O').addClass('oBox').removeClass('open');
+          getWinner('xBox');
 
-          rowWinner('oBox');
-          colWinner('oBox');
-          diagWinner('oBox');
+          // if turncount is an odd number play o
+        } else if(_this.turncount % 2 !== 0 && $(this).hasClass('open')) {
+            $(this).html('<p>O</p>').addClass('oBox').removeClass('open');
+            getWinner('oBox');
 
         } else {
           console.log("error");
@@ -84,35 +86,33 @@ $(document).ready(function() {
         console.log(_this.turncount);
 
         // if turncount === 9 && !rowWinner || !colWinner || !diagWinner
-        // console.log("It's a tie")
         if(_this.turncount === 9) {
           $gameMsgDiv.html("It's a tie");
         }
 
                 
       }); // end on click
-      
+      // reset game, remove classes and add open class, reset turncount to 0
      $('#reset').on('click', function() {
         $('.box').html("&nbsp;").removeClass('xBox oBox').addClass('open');
         _this.turncount = 0;
+        $gameMsgDiv.html("");
         
       });          
     };
     
-    // A starter Player constructor.
+    // Player constructor
     function Player(team) {
         this.team = team;
         this.wins = 0;
     };
     
-    // A starter Board constructor.
+    // Board constructor
     function Board() {
         this.cells = $('#board-container .box');
         this.cells.addClass('open');
     };
-    
-
-    
+       
     var game = new Game();
     
     game.play();
